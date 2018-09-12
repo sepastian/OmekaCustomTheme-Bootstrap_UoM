84672cc3e8bcd2b2c8afcd0e064ad8a853837b60
Sebastian Gassner <sebastian.gassner@uni-passau.de>
2018-09-12 17:29:47 +0200

    More tiles.

--- javascripts/up.js
+++ javascripts/up.js
@@ -1,19 +1,147 @@
 var tiles = {
     // Layer 0
+    "0-1": "/files/square_thumbnails/37731211bdde20e58feb08413bac0089.jpg",
+    "0-2": "/files/square_thumbnails/be4c6e0770b719d88e6f7143694bc972.jpg",
 
     // Layer 1
-    "1-1": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/21d7445429a759c8a369beabf581024e.jpg",
-    "1-2": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/688550b26d959134207dfecec6ada8d2.jpg",
-    "1-3": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/110f5bd92997fd7bdb17324ee905e30b.jpg",
-    "1-4": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/d8de70f47296b25232786736b2e45ec4.jpg",
-    "1-5": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/c4456f479e4126354e2ff188572dd077.jpg",
-    "1-6": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/97bed54fcdf5bcda14750902ef3228ac.jpg",
+    // Uni-Geschichte
+    "1-1": "/files/square_thumbnails/21d7445429a759c8a369beabf581024e.jpg",
+    // Lehre im Wandel
+    "1-2": "/files/square_thumbnails/688550b26d959134207dfecec6ada8d2.jpg",
+    // Studentenschaft
+    "1-3": "/files/square_thumbnails/110f5bd92997fd7bdb17324ee905e30b.jpg",
+    // Aussenwirkung
+    "1-4": "/files/square_thumbnails/d8de70f47296b25232786736b2e45ec4.jpg",
+    // Campus
+    "1-5": "/files/square_thumbnails/c4456f479e4126354e2ff188572dd077.jpg",
+    // Projekt
+    "1-6": "/files/square_thumbnails/97bed54fcdf5bcda14750902ef3228ac.jpg",
 
     // Layer 2
-    "1-1-1": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/21d7445429a759c8a369beabf581024e.jpg",
-    "1-1-2": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/ed5d989590391a136a333b25a8140214.jpg",
-    "1-1-3": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/806370884804db9808b68b257835daed.jpg",
-    "1-1-4": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/7ab7d7cabcb90fe2d849ea78bfa0c164.jpg",
-    "1-1-5": "http://dighum01.phil.uni-passau.de:8088/files/square_thumbnails/4065dd20999c207a2a16a3d7d02de726.jpg",
+    // Uni-Geschichte
+    "1-1-1": "/files/square_thumbnails/21d7445429a759c8a369beabf581024e.jpg",
+    "1-1-2": "/files/square_thumbnails/ed5d989590391a136a333b25a8140214.jpg",
+    "1-1-3": "/files/square_thumbnails/806370884804db9808b68b257835daed.jpg",
+    "1-1-4": "/files/square_thumbnails/7ab7d7cabcb90fe2d849ea78bfa0c164.jpg",
+    "1-1-5": "/files/square_thumbnails/4065dd20999c207a2a16a3d7d02de726.jpg",
+    // Lehre im Wandel
 
+    // Studentenschaft
+    "1-3-1": "/files/square_thumbnails/df9110be9395bfc8c8e4f6e8e327c397.jpg",
+    "1-3-2": "/files/square_thumbnails/057b342a5bec039889c3fbc6c022c244.jpg",
+    "1-3-3": "/files/square_thumbnails/2f39545c9b72357853a60a343bf69ba8.jpg"
+
+    // Aussenwirkung
+
+    // Campus
+
+    // Projekt
 }
+
+/*
+Uni-Geschichte
+
+* Uni Geschichte: [317](/files/square_thumbnails/21d7445429a759c8a369beabf581024e.jpg)
+
+ * Wie die Uni wurde, was sie ist: Datei #317: “Noten_Register 1833_34 bis 37_38"
+/files/square_thumbnails/21d7445429a759c8a369beabf581024e.jpg
+
+  * Die Jesuiten und die Bildung: [582] /files/square_thumbnails/ed5d989590391a136a333b25a8140214.jpg
+Datei #582: “UAP_FBA_B_Rechnungen_1785-1786_04-05.jpg”
+
+  * Die Säkularisation und ihre Folgen: Datei #334: “18-Säk-Liste-1803-Seite-18.jpg”
+/files/square_thumbnails/806370884804db9808b68b257835daed.jpg
+
+  * Stagnation und Neuanfang: Datei #945: “Seite003hinten.jpg”
+/files/square_thumbnails/7ab7d7cabcb90fe2d849ea78bfa0c164.jpg
+
+  * Der Campus nimmt Gestalt an: Datei #72: “PNP_Sonderbeilage_1978_Campusplan"
+/files/square_thumbnails/4065dd20999c207a2a16a3d7d02de726.jpg
+
+* Spiegel der Hochschulgeschichte: Datei #743: “Aufkleber mit dem Siegel der Universität Passau"
+/files/square_thumbnails/688550b26d959134207dfecec6ada8d2.jpg
+
+  * Der Jahresbericht 1932/33: Datei #301: “Seite 9 des Jahresberichtes 1932/33”
+/files/square_thumbnails/26482cc2992af1070c1da3cfd9850ca6.jpg
+
+  * Nachrichten und Veröffentlichungen: Datei #747: “Extrahiertes Bild (Nr. 2) aus Vordere Bilderseite aus Nachrichten und Veröffentlichungen (1.
+Ausgabe)”
+/files/square_thumbnails/b2fea2d3f35fda304a0100a92cfe739e.jpg
+
+ * Warum Passau?: Datei #830: “Seite47,002.JPG” (Denkschrift, S. 47)
+/files/square_thumbnails/110f5bd92997fd7bdb17324ee905e30b.jpg
+
+  * Überblick: Timeline → Grafik/nur Schrift?
+  * Denkschrift: Objekt #338: "Denkschrift_Cover"
+/files/square_thumbnails/7abf49a756ede009884813af70c96f02.jpg
+
+  * Ergänzung zur Denkschrift: Datei #855: “EzD_13.JPG”
+/files/square_thumbnails/5925672c913868a4634e1f8a680918a2.jpg
+
+  * Standortuntersuchung: Objekt #339: "Besitzstandskarte"
+/files/square_thumbnails/a8e0ca17a325a9b996e39964b8c81365.jpg
+  * Förderer: → kann so bleiben
+
+* Lehre im Wandel: XX
+ * Lehren und Lernen im 20. Jahrhundert: Objekt #161: "Vorlesungsverzeichnis Sommerhalbjahr 1951"
+/files/square_thumbnails/3d8d640f03d5aa938a636fd5d1ed7b6a.jpg
+
+  * Einzelthemen→ jeweils erstes Bild aus Rubriken
+/files/square_thumbnails/03f0225ca8328520d54931dbeab94b4b.jpg
+/files/square_thumbnails/607cf9419e6211de29a9576db5277c04.jpg
+/files/square_thumbnails/3d8d640f03d5aa938a636fd5d1ed7b6a.jpg
+/files/square_thumbnails/bf7ca0ac859f03742a0b3141ea6961b7.jpg
+
+  * Timeline, Zeitzeugen → Grafik/nur Schrift?
+  * Oral History: Objekt #237: "Foto von Herrn Mario Puhane“
+/files/square_thumbnails/a3b60c4dd04349cb81f00e60d7769959.jpg
+
+Studentenschaft: Datei #284: “Paul Esterl Absolutorium Streiflichtaufnahme Siegel"
+/files/square_thumbnails/057b342a5bec039889c3fbc6c022c244.jpg
+
+- Studentenleben im 19. Jahrhundert
+Datei #523: “Seite 1.jpg” (Statuten, S. 1)
+/files/square_thumbnails/df9110be9395bfc8c8e4f6e8e327c397.jpg
+- Paul Esterl
+Datei #284: “Paul Esterl Absolutorium Streiflichtaufnahme Siegel"
+/files/square_thumbnails/057b342a5bec039889c3fbc6c022c244.jpg
+- Die Weltkriege, die Studenten
+Objekt #303: "Inscriptionszettel 1916/17"
+/files/square_thumbnails/2f39545c9b72357853a60a343bf69ba8.jpg
+
+Außenwirkung: Datei #743: “Aufkleber mit dem Siegel der Universität Passau"
+/files/square_thumbnails/688550b26d959134207dfecec6ada8d2.jpg
+- Fernliegend?:
+Datei #957: “1.1 Foto-Unterschrift_JPEG.jpg” (Partnerschaft Tours)
+/files/square_thumbnails/d8de70f47296b25232786736b2e45ec4.jpg
+- Was war wichtig?
+Objekt #45: "Titelblatt PNP Sonderbeilage 1983
+/admin/files/show/82
+- Das Logo
+Datei #413: “capture000011.jpg” (Rubrik Siegel: Münze mit Madonna)
+/files/square_thumbnails/cfb38f38b6c9e5328b897550c45859b5.jpg
+- Born Digital
+Datei #1069: “FIM_1_1998.JPG”
+/files/square_thumbnails/22739f1799af2365bdea22c909d992d2.jpg
+
+Campus: Datei #752: “Kommilitonen im Gespräch vor den Hörsälen im Philosophicum"
+/files/square_thumbnails/c4456f479e4126354e2ff188572dd077.jpg
+- Besondere Orte:
+Datei #752: “Kommilitonen im Gespräch vor den Hörsälen im Philosophicum"
+/files/square_thumbnails/c4456f479e4126354e2ff188572dd077.jpg
+- Meine Kunst am Campus
+Datei #1352: “IMG_2833.JPG”
+/admin/files/show/1349
+- Die Stadt und die Uni
+Datei #1318: “Passauräumtauf.jpg”
+/files/square_thumbnails/60a8cf70f354f38a02cd0c22ffead294.jpg
+
+Projekt: Datei #280: “01-Säk-Liste-1803-Seite-01.jpg”
+/files/square_thumbnails/97bed54fcdf5bcda14750902ef3228ac.jpg
+- Das Projekt
+Objekt #306: "Labor"
+/files/square_thumbnails/ee0792235a4424ac04f74511149f3d0f.jpg
+- Ein Archiv packt aus
+Datei #280: “01-Säk-Liste-1803-Seite-01.jpg”
+/files/square_thumbnails/97bed54fcdf5bcda14750902ef3228ac.jpg
+*/
